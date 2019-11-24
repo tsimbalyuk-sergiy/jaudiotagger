2dbdf6f4bd0510a5403138949cfade6863f9bb47
Sergiy tsimbalyuk <sv.tsimbalyuk@gmail.com>
2019-11-24 16:40:20 +0200

    module-info.java updated

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,4 +3,19 @@ module org.jaudiotagger {
   requires java.desktop;
 
   exports org.jaudiotagger;
+  exports org.jaudiotagger.logging;
+  exports org.jaudiotagger.audio.mp3;
+  exports org.jaudiotagger.audio.flac;
+  exports org.jaudiotagger.audio.flac.metadatablock;
+  exports org.jaudiotagger.audio.exceptions;
+  exports org.jaudiotagger.audio.wav;
+  exports org.jaudiotagger.audio.wav.chunk;
+  exports org.jaudiotagger.audio.generic;
+  exports org.jaudiotagger.tag.flac;
+  exports org.jaudiotagger.tag.wav;
+  exports org.jaudiotagger.tag.id3;
+  exports org.jaudiotagger.tag.id3.framebody;
+  exports org.jaudiotagger.tag.id3.reference;
+  exports org.jaudiotagger.tag.id3.valuepair;
+  exports org.jaudiotagger.tag.reference;
 }
